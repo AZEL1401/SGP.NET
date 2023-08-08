7315cb8bac8c84a090a14748c8efe5e58d314250
Ali Sepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2023-08-08 06:39:45 +0330

    Terra In Tehran (-90) Live Coordinate Showing

--- SGPSandbox/Program.cs
+++ SGPSandbox/Program.cs
@@ -17,15 +17,16 @@ namespace SGPSandbox
 	{
 		static void Main(string[] args)
 		{
-			var tleUrl = new Uri("https://celestrak.com/NORAD/elements/visual.txt");
+			var tleUrl = new Uri("https://celestrak.org/NORAD/elements/resource.txt");
+			var lp = new LocalTleProvider(true, "C:\\Users\\Ali\\Desktop\\resource.txt");
 			var provider = new RemoteTleProvider(true, tleUrl);
-			var tles = provider.GetTles();
+			var tles = lp.GetTles().Where(x => x.Value.Name.ToUpper() == "TERRA");
 			var satellites = tles.Select(pair => new Satellite(pair.Value)).ToList();
 
-			var minAngle = (Angle)10;
-			var gs = new GroundStation(new GeodeticCoordinate(30.229777, -81.617525, 0));
+			var minAngle = (Angle)(-90);
+			var gs = new GroundStation(new GeodeticCoordinate(35.7698135, 51.4447732, 1.5));
 
-			var state = TrackingState.ListingComPorts;
+			var state = TrackingState.ListingVisible;
 			Satellite tracking = null;
 			SerialPort comPort = null;
 
@@ -33,22 +34,22 @@ namespace SGPSandbox
 			{
 				switch (state)
 				{
-					case TrackingState.ListingComPorts:
-						comPort = SelectComPort();
-						if (comPort != null)
-						{
-							if (comPort.IsOpen)
-								comPort.Close();
-							comPort.Open();
+					//case TrackingState.ListingComPorts:
+					//	comPort = SelectComPort();
+					//	if (comPort != null)
+					//	{
+					//		if (comPort.IsOpen)
+					//			comPort.Close();
+					//		comPort.Open();
 
-							comPort.WriteLine("$G");
-							comPort.WriteLine("G10P0L20X0Y0Z0");
-						}
+					//		comPort.WriteLine("$G");
+					//		comPort.WriteLine("G10P0L20X0Y0Z0");
+					//	}
 
-						state = TrackingState.ListingVisible;
-						break;
+					//	state = TrackingState.ListingVisible;
+					//	break;
 					case TrackingState.ListingVisible:
-						tracking = SelectVisibleSatellite(satellites, gs, minAngle);
+						tracking = SelectVisibleSatellite(satellites, gs, minAngle, DateTime.UtcNow);
 						state = TrackingState.Tracking;
 						break;
 					case TrackingState.Tracking:
@@ -57,14 +58,14 @@ namespace SGPSandbox
 						if (PressedKey(ConsoleKey.Q))
 							state = TrackingState.Quitting;
 
-						Console.Clear();
+						//Console.Clear();
 						var observation = gs.Observe(tracking, DateTime.UtcNow);
 						Console.WriteLine(tracking.Name);
-						Console.WriteLine($"{observation.Elevation.Degrees:F2};{observation.Azimuth.Degrees:F2}");
+						Console.WriteLine($"{observation.Elevation.Degrees:F4};{observation.Azimuth.Degrees:F4}");
 
-						comPort?.WriteLine($"G1X{-observation.Elevation.Degrees:F2}Y{observation.Azimuth.Degrees:F2}F1000");
+						comPort?.WriteLine($"G1X{-observation.Elevation.Degrees:F4}Y{observation.Azimuth.Degrees:F4}F1000");
 
-						Thread.Sleep(250);
+						Thread.Sleep(10);
 						break;
 				}
 
@@ -110,7 +111,7 @@ namespace SGPSandbox
 			return key.Key == needle;
 		}
 
-		private static Satellite SelectVisibleSatellite(List<Satellite> satellites, GroundStation gs, Angle minAngle)
+		private static Satellite SelectVisibleSatellite(List<Satellite> satellites, GroundStation gs, Angle minAngle, DateTime dt)
 		{
 			var visible = new List<Satellite>();
 			visible.Clear();
@@ -118,8 +119,9 @@ namespace SGPSandbox
 
 			foreach (var satellite in satellites)
 			{
-				var satPos = satellite.Predict();
-				if (gs.IsVisible(satPos, minAngle, satPos.Time))
+				var satPos = satellite.Predict(dt);
+				var x = satPos.Observe(gs.Location, dt);
+				//if (gs.IsVisible(satPos, minAngle, satPos.Time))
 					visible.Add(satellite);
 			}
 
